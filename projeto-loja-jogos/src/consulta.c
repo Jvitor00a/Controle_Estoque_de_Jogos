99c8ba1f6902ee9f10132096749aedeee4804f9e
Alexandre Soares <pedrozaalexandre@gmail.com>
2024-07-15 13:18:09 -0300

    Modificacoes tela consulta

--- projeto-loja-jogos/src/consulta.c
+++ projeto-loja-jogos/src/consulta.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include "raygui.h"
 #include "raylib.h"
 #include "consulta.h"
 
 Produto buscarProdutoPorID(int id)
 {
+    printf("Pesquisando produto %d\n", id);
+
     FILE *file = fopen("data/produtos.txt", "r");
     Produto produto;
     Produto produtoNaoEncontrado = {0, "", "", 0.0, 0};
@@ -70,19 +74,33 @@ void exibirProduto(Produto produto)
     }
 }
 
-bool editandoCaixaPesquisa;
-char textoCaixaPesquisa[1000];
+// Function to check if a string is a number
+bool isNumber(const char *str)
+{
+    while (*str)
+    {
+        if (!isdigit(*str))
+        {
+            return false;
+        }
+        str++;
+    }
+    return true;
+}
 
+// Textos constantes parte da interface
 const char *EtiquetaId = "Id";
 const char *EtiquetaNome = "Nome";
 const char *EtiquetaCategoria = "Categoria";
 const char *EtiquetaValor = "Valor";
 const char *EtiquetaQtd = "Quantidade";
 
-char TextoLabelPesquisando[128] = "Pesquisando....";
-bool WindowBox000Active = true;
-bool TextBox001EditMode = false;
+// Variáveis da interface
+char TextoLabelPesquisando[128] = "Digite o termo de busca na caixa de texto acima";
+bool EditandoCaixaTextoPesquisa = false;
 char TextoCaixaPesquisa[128] = "Digite para pesquisar";
+bool ProdutoEncontradoComSucesso = false;
+// Variáveis do produto pesquisado
 char IdProduto[128] = "001";
 char NomeProduto[128] = "Nome do produto";
 char CategoriaProduto[128] = "Categoria1";
@@ -94,19 +112,61 @@ void renderizarRotaDePesquisa()
     // raygui: controls drawing
     //----------------------------------------------------------------------------------
 
-    if (GuiTextBox((Rectangle){8, 32, 584, 24}, TextoCaixaPesquisa, 128, TextBox001EditMode))
-        TextBox001EditMode = !TextBox001EditMode;
-    GuiLabel((Rectangle){272, 56, 120, 24}, TextoLabelPesquisando);
-    GuiLabel((Rectangle){240, 96, 16, 24}, EtiquetaId);
-    GuiLabel((Rectangle){224, 144, 32, 24}, EtiquetaNome);
-    GuiLabel((Rectangle){200, 192, 56, 24}, EtiquetaCategoria);
-    GuiLabel((Rectangle){224, 240, 32, 24}, EtiquetaValor);
-    GuiLabel((Rectangle){192, 288, 64, 24}, EtiquetaQtd);
-    GuiTextBox((Rectangle){256, 96, 120, 24}, IdProduto, 128, false);
-    GuiTextBox((Rectangle){256, 144, 120, 24}, NomeProduto, 128, false);
-    GuiTextBox((Rectangle){256, 192, 120, 24}, CategoriaProduto, 128, false);
-    GuiTextBox((Rectangle){256, 240, 120, 24}, ValorProduto, 128, false);
-    GuiTextBox((Rectangle){256, 288, 120, 24}, QtdProduto, 128, false);
+    if (GuiTextBox((Rectangle){8, 48, 584, 24}, TextoCaixaPesquisa, 128, EditandoCaixaTextoPesquisa))
+    {
+        EditandoCaixaTextoPesquisa = !EditandoCaixaTextoPesquisa;
 
+        // Search logic
+        Produto produtoEncontrado;
+        if (isNumber(TextoCaixaPesquisa))
+        {
+            int id = atoi(TextoCaixaPesquisa);
+            produtoEncontrado = buscarProdutoPorID(id);
+        }
+        else
+        {
+            produtoEncontrado = buscarProdutoPorNome(TextoCaixaPesquisa);
+        }
+
+        if (produtoEncontrado.id != 0)
+        {
+            snprintf(IdProduto, sizeof(IdProduto), "%d", produtoEncontrado.id);
+            snprintf(NomeProduto, sizeof(NomeProduto), "%s", produtoEncontrado.nome);
+            snprintf(CategoriaProduto, sizeof(CategoriaProduto), "%s", produtoEncontrado.categoria);
+            snprintf(ValorProduto, sizeof(ValorProduto), "%.2f", produtoEncontrado.preco);
+            snprintf(QtdProduto, sizeof(QtdProduto), "%d", produtoEncontrado.quantidade);
+            strcpy(TextoLabelPesquisando, "Produto encontrado!");
+            ProdutoEncontradoComSucesso = true;
+        }
+        else
+        {
+            strcpy(TextoLabelPesquisando, "Produto não encontrado.");
+            ProdutoEncontradoComSucesso = false;
+        }
+    }
+    // Ícone lupa
+    GuiLabel((Rectangle){568, 48, 584, 24}, "#42#");
+
+    const int screenWidth = 584;
+    const int tamanhoLabelPesquisando = strlen(TextoLabelPesquisando) * 6;
+    GuiLabel((Rectangle){(screenWidth - tamanhoLabelPesquisando) / 2, 80, tamanhoLabelPesquisando, 24}, TextoLabelPesquisando);
+
+    if (ProdutoEncontradoComSucesso)
+    {
+        GuiLabel((Rectangle){240, 120, 16, 24}, EtiquetaId);
+        GuiTextBox((Rectangle){256, 120, 120, 24}, IdProduto, 128, false);
+
+        GuiLabel((Rectangle){224, 168, 32, 24}, EtiquetaNome);
+        GuiTextBox((Rectangle){256, 168, 120, 24}, NomeProduto, 128, false);
+
+        GuiLabel((Rectangle){200, 216, 56, 24}, EtiquetaCategoria);
+        GuiTextBox((Rectangle){256, 216, 120, 24}, CategoriaProduto, 128, false);
+
+        GuiLabel((Rectangle){224, 264, 32, 24}, EtiquetaValor);
+        GuiTextBox((Rectangle){256, 264, 120, 24}, ValorProduto, 128, false);
+
+        GuiLabel((Rectangle){192, 312, 64, 24}, EtiquetaQtd);
+        GuiTextBox((Rectangle){256, 312, 120, 24}, QtdProduto, 128, false);
+    }
     //----------------------------------------------------------------------------------
 }
